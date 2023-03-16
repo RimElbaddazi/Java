6a1217e0ce06933c0074ed98e05933038fe81c5e
Rim <etu_elbaddazi.rim@emsi-edu.ma>
2023-03-16 17:56:29 +0000

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("je suis Rim ELBADDAZI ");
+
+	}
+
+}
